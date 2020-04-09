cf23695fccc83141518746633a2fcc4e527fc232
Vinodh Samuel <samuelvinodh@gmail.com>
2020-04-09 19:11:41 -0400

    Initial Spring

---
+++ ppmtoolSpring/src/main/java/com/sam/react/PpmtoolSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sam.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolSpringApplication.class, args);
+	}
+
+}
---
+++ ppmtoolSpring/src/test/java/com/sam/react/PpmtoolSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
